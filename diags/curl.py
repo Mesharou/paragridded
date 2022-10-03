e310499047a4679479fd0e3c2d4d6edeaa0bbcbd
gula <Mesharou@gmail.com>
2022-10-03 15:51:25 -0700

    Use real 2d dx,dy for curl

--- diags/curl.py
+++ diags/curl.py
@@ -27,27 +27,31 @@ class Curl:
             keeptoplevel(u)
             keeptoplevel(v)
 
-        f = var.new("f")
-        self.reader.read(f)
-
-        dx = 1e3
-        tasks = iter([(u[tile], v[tile], f[tile], dx)
+        f = var.new("f"); self.reader.read(f)
+        pm = var.new("pm"); self.reader.read(pm)
+        pn = var.new("pn"); self.reader.read(pn)
+        
+        tasks = iter([(u[tile], v[tile], f[tile], pm[tile], pn[tile])
                       for tile in var])
         print(f"compute curl {var.varname}")
         with mp.Pool(processes=self.nthreads) as pool:
             data = pool.starmap(curl2d_overf, tasks)
 
         var.update(data)
-
         var.staggering.horiz = "f"
 
 
 @jit
-def curl2d_overf(u, v, f, dx):
+def curl2d_overf(u, v, f, pm, pn):
     ny, nx = u.shape
     vor = np.zeros(u.shape, dtype=u.dtype)
     for j in range(ny-1):
         for i in range(nx-1):
-            cff = 1./(f[j, i]*dx)
-            vor[j, i] = (v[j, i+1]-u[j+1, i+1]-v[j, i]+u[j, i])*cff
+            cff = 1./(f[j, i])
+            vor[j, i] = ( (v[j, i+1]-v[j, i])\
+                          * 0.25 * (pm[j+1, i]+pm[j, i+1]+pm[j+1, i+1]+pm[j, i])\
+                         -(u[j+1, i]-u[j, i])\
+                          * 0.25 * (pn[j+1, i]+pn[j, i+1]+pn[j+1, i+1]+pn[j, i])\
+                        ) * cff
+            
     return vor
